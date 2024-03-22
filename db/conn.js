c271b823db762c810a3ae4105c567fcfad38428a
MatheusBeneti <matheusbeneti3@gmail.com>
2024-03-22 12:15:36 -0300

    configurando db

--- db/conn.js
+++ db/conn.js
@@ -1,3 +1,35 @@
+const password = 'admin';
+
+const { default: PasswordPrompt } = require('inquirer/lib/prompts/password');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://admin:${password}@cluster0.rokvirh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+      // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      print('connecting to MongoDB')
+    } finally {
+      // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+//----------------------------------------------------------------
 const mongoose = require('mongoose')
 const url = 'localhost'
 
